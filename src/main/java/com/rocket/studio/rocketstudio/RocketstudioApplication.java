2d2958b345de89771597922790f7b92a7e27e0cf
Santiago <santiagobinario@gmail.com>
2024-06-08 16:01:49 -0600

    update

---
+++ src/main/java/com/rocket/studio/rocketstudio/RocketstudioApplication.java
@@ -0,0 +1,13 @@
+package com.rocket.studio.rocketstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocket/studio/rocketstudio/RocketstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.studio.rocketstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
